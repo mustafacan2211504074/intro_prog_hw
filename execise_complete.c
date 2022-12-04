625139a3a2b89bd2e6ad6e41cb6918f664992ece
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-04 15:05:41 +0300

    Update execise_complete.c

--- execise_complete.c
+++ execise_complete.c
@@ -2,17 +2,40 @@
 #include "stdlib.h"
 
 
-class Solution {
-    double power(double x, int n){
-        if(n == 0) return 1.0;
-        if(n == 1) return x;
-        if(n < 0) return power(1/x, -n);
-        double result = power(x*x, n/2);
-        if(n%2) result *= x;
-        return result;
+void solution(int *arr, int n, int k)
+//find the solution
+{
+    int i, j, temp;
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
     }
-        public:
-        double myPow(double x, int n) {
-        return power(x, n);
+    printf("%d", arr[k - 1]);
+}
+
+int main()
+{
+    int n, k, i;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d %d", &n, &k);
+    int *arr = (int *)malloc(n * sizeof(int));
+    for (i = 0; i < n; i++)
+    {
+        printf("Enter the element %d: ", i + 1);
+        scanf("%d", &arr[i]);
     }
-};
+    solution(arr, n, k);
+    return 0;
+}
+
+
+
+
